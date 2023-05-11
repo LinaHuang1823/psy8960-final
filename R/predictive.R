b08480e7f891214911be1b82c45001b20891499e
Lina Huang <huan2384@umn.edu>
2023-05-10 20:22:17 -0500

    predictive.R update

--- R/predictive.R
+++ R/predictive.R
@@ -4,35 +4,35 @@ library(tidyverse)
 library(caret)
 library(haven)
 
-# Read the dataset from the CSV file
+# Data Import and Cleaning
 predict_dataset<- read.csv("../data/combined_dataset.csv")
 
+# Analysis
+# Create training and testing datasets using the sampled indices
 train_cases <- sample(1:nrow(predict_dataset))
-
-gss_train_tbl <- predict_dataset[train_cases, ]
-gss_test_tbl <- predict_dataset[-train_cases, ]
+train_tbl <- predict_dataset[train_cases, ]
+test_tbl <- predict_dataset[-train_cases, ]
 
 # Identify factor variables with only one level
 single_level_factors <- sapply(predict_dataset, function(x) is.factor(x) && length(levels(x)) < 2)
 
-# Remove those variables
+# Remove those single-level factor variables from the dataset
 predict_dataset <- predict_dataset[, !single_level_factors]
-
-
-training_folds <- createFolds(gss_train_tbl$Attrition,
+# Create 10-fold cross-validation indices for the training dataset
+training_folds <- createFolds(train_tbl$Attrition,
                               k=10)
-#dummy coding 
-gss_train_tbl$Attrition <- ifelse(gss_train_tbl$Attrition == "Yes", 1, 0)
+# Dummy coding the Attrition variable (convert "Yes" to 1 and "No" to 0)
+train_tbl$Attrition <- ifelse(train_tbl$Attrition == "Yes", 1, 0)
 
 # Identify columns with one level
-single_level_cols <- sapply(gss_train_tbl, function(col) length(unique(col)) == 1)
-
+single_level_cols <- sapply(train_tbl, function(col) length(unique(col)) == 1)
 # Convert single-level columns to factors with two levels
-gss_train_tbl[single_level_cols] <- lapply(gss_train_tbl[single_level_cols], factor, levels = c("level1", "level2"))
+train_tbl[single_level_cols] <- lapply(train_tbl[single_level_cols], factor, levels = c("level1", "level2"))
 
+# Train a linear regression model (Model 1)
 model1 <- train(
   Attrition ~ .,
-  gss_train_tbl, 
+  train_tbl, 
   method="lm",
   na.action=na.pass,
   preProcess=c("center", "scale", "nzv", "medianImpute"),
@@ -40,9 +40,10 @@ model1 <- train(
 )
 model1
 
+# Train a generalized linear model with elastic net regularization (Model 2)
 model2 <- train(
   Attrition ~ .,
-  gss_train_tbl, 
+  train_tbl, 
   method="glmnet",
   tuneLength=3,
   na.action=na.pass,
@@ -51,9 +52,10 @@ model2 <- train(
 )
 model2
 
+# Train a random forest model (Model 3)
 model3 <- train(
   Attrition ~ .,
-  gss_train_tbl, 
+  train_tbl, 
   method="ranger",
   tuneLength=3,
   na.action=na.pass,
@@ -62,9 +64,10 @@ model3 <- train(
 )
 model3
 
+# Train an XGBoost model (Model 4)
 model4 <- train(
   Attrition ~ .,
-  gss_train_tbl, 
+  train_tbl, 
   method="xgbTree",
   tuneLength=3,
   na.action=na.pass,
@@ -73,16 +76,15 @@ model4 <- train(
 )
 model4
 
+# Publication
+#create summary table and plot
 summary(resamples(list(model1, model2, model3, model4)))
 resample_sum <- summary(resamples(list(model1, model2, model3, model4)))
 dotplot(resamples(list(model1, model2, model3, model4)))
-
-# Publication
 # Get the mean values of each metric for each model
 mean_MAE <- resample_sum $statistics$MAE[, "Mean"]
 mean_RMSE <- resample_sum $statistics$RMSE[, "Mean"]
 mean_Rsquared <- resample_sum$statistics$Rsquared[, "Mean"]
-
 # Create the model_comparison data frame
 model_comparison <- data.frame(
   Model = c("Model1", "Model2", "Model3", "Model4"),
@@ -92,7 +94,7 @@ model_comparison <- data.frame(
 )
 # Set the row names of the data frame to the model names
 rownames(model_comparison) <- model_comparison$Model
-# Remove the "Model" column
+# Remove the "Model" column for simplicity and clarity
 model_comparison <- model_comparison[, -1]
 # Display the modified model_comparison table
 print(model_comparison)
@@ -102,38 +104,28 @@ print(model_comparison)
 #XGBoost is a powerful ensemble method that combines multiple decision trees 
 #and can handle complex relationships between features and the target variable. 
 #This improved performance is reflected in the table.Based on the results in the table, 
-#Model4 has the best predicted performance.This is because Model4 has the lowest 
-#Mean Absolute Error (MAE) and Mean RootMean Squared Error (RMSE) values, indicating better prediction accuracy. 
-#Additionally, Model4 has the highest Mean R-squared value, indicating that it 
-#can explain the largest proportion of variance in the dependent variable (Attrition) 
-#compared to the other models.
+#Model4 (XGBoost) has the best predicted performance.This is because Model4 has the lowest 
+#Mean Absolute Error (Mean_MAE=0.09) and Mean RootMean Squared Error (RMSE=0.15) values, indicating better prediction accuracy. 
+#Additionally,the Mean R-squared value of Model4 is pretty big, indicating that it 
+#can explain the large proportion of variance in the dependent variable (Attrition) 
+#compared to the other models. In addition, xgbTree (XGBoost) have better ability to
+#handle missing data and imbalanced datasets.In addtion,XGBoost can well prevent overfitting problem.
 
-library(tidyverse)
-library(caret)
-library(haven)
+#Part 2-2:What is the incremental predictive accuracy gained by including text 
+#data in your model versus not including text data?
 
-# Read the dataset from the CSV file
-predict_dataset <- read.csv("../data/combined_dataset.csv")
+# Identify columns with less than two unique values
+low_var_cols <- sapply(train_tbl, function(col) length(unique(col)) < 2)
 
-train_cases <- sample(1:nrow(predict_dataset))
+# Remove those columns with less than two unique values
+#note: I deleted columns with less than two unique values since contrasts can be 
+#applied only to factors with 2 or more levels. Otherwise, it will throw me error. 
+train_tbl <- train_tbl[, !low_var_cols]
 
-gss_train_tbl <- predict_dataset[train_cases, ]
-gss_test_tbl <- predict_dataset[-train_cases, ]
-
-# Remove columns with only one level
-#single_level_factors <- sapply(predict_dataset, function(x) is.factor(x) && length(levels(x)) < 2)
-#predict_dataset <- predict_dataset[, !single_level_factors]
-
-# Create training folds
-training_folds <- createFolds(gss_train_tbl$Attrition, k=10)
-
-# Dummy coding
-gss_train_tbl$Attrition <- ifelse(gss_train_tbl$Attrition == "Yes", 1, 0)
-
-# Model without text-derived predictors
+# Model without text predictors
 model_no_text <- train(
-  Attrition ~ . - Text_Feature, #edit  later..........
-  gss_train_tbl,
+  Attrition ~ . - bad - good,
+  train_tbl,
   method="glmnet",
   tuneLength=3,
   na.action=na.pass,
@@ -142,10 +134,10 @@ model_no_text <- train(
 )
 model_no_text
 
-# Model with text-derived predictors
+# Model with text predictors
 model_with_text <- train(
   Attrition ~ .,
-  gss_train_tbl,
+  train_tbl,
   method="glmnet",
   tuneLength=3,
   na.action=na.pass,
@@ -154,3 +146,29 @@ model_with_text <- train(
 )
 model_with_text
 
+# Publication
+#create summary table and plot
+summary(resamples(list(model_no_text, model_with_text)))
+resample_sum <- summary(resamples(list(model_no_text, model_with_text)))
+dotplot(resamples(list(model_no_text, model_with_text)))
+# Get the mean values of each metric for each model
+mean_MAE <- resample_sum $statistics$MAE[, "Mean"]
+mean_RMSE <- resample_sum $statistics$RMSE[, "Mean"]
+mean_Rsquared <- resample_sum$statistics$Rsquared[, "Mean"]
+# Create the model_comparison data frame
+model_comparison <- data.frame(
+  Model = c("Model_no_text", "model_with_text" ),
+  Mean_MAE = mean_MAE,
+  Mean_RMSE = mean_RMSE,
+  Mean_Rsquared = mean_Rsquared
+)
+# Set the row names of the data frame to the model names
+rownames(model_comparison) <- model_comparison$Model
+# Remove the "Model" column for simplicity and clarity
+model_comparison <- model_comparison[, -1]
+# Display the modified model_comparison table
+print(model_comparison)
+
+#Since the Mean Mean Absolute Error (MAE), mean RMSE and mean R square are almost the same for both models,
+#the differences in predictive accuracy between the model with text data and the model without text data are very small.
+#This suggests that including the text data doesn't contribute much to improving the predictive accuracy of the model.
